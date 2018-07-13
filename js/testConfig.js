2ba0d207d971a7c8419877a47b7996f1affe4096
MLY123 <1753868366@qq.com>
2018-07-13 11:47:28 +0800

    21w

---
+++ js/testConfig.js
@@ -0,0 +1,3 @@
+var _testChsiUrl_ = "http://192.168.1.27:8080/xhlc-cif-oms/test/chsi/api";
+var _testThirdUsrId_ = "thirdUsrID_test0703";
+var _testIpcrsUrl_ = "http://192.168.1.27:8080/xhlc-cif-oms/test/ipcrs/api";
